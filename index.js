df992ea5e640ab498cfce40a48beccb0a8fd207d
Luis Pestana <lpestana@protonmail.com>
2020-02-16 02:01:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // add solution here
-let theBeatlesplay = (musicians, instruments) => {
+let theBeatlesPlay = (musicians, instruments) => {
   let arr = [];
   for (let i = 0; i < musicians.length; i++) {
     arr.push(`${musicians[i]} plays ${instruments[i]}`);
